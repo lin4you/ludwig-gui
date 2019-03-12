a66425658079436d8bd95556c096188f9274b4f8
Friedrich Waack <ubuntuuser1994!>
2019-03-12 17:56:26 +0100

    install vue, vuex and vuetify and added default application markup

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+
+mix.js('renderer.js', 'dist');
+
+/*
+mix.sass('assets/style.scss', 'dist')
+    .js('assets/app.js', 'dist');
+ */
